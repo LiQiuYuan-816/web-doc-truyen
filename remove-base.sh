cdd63b70c38a4915703a4edd09e7c7580bfc3262
LiQiuYuan <dinhtieuca5218946@gmail.com>
2026-02-09 02:12:19 +0700

    Fix back links in story index.html

--- remove-base.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-find . -name "*.html" | while read file; do
-  if grep -qi "<base " "$file"; then
-    sed -i '/<base /Id' "$file"
-    echo "REMOVED <base>: $file"
-  fi
-done
